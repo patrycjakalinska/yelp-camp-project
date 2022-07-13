aca93b969acd1fa3ce843c425704a785998511d9
patrycjakalinska <kalinska@student.agh.edu.pl>
2022-07-13 20:33:26 +0200

    Add boilerplate, partials and card view on main page

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,13 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 10) +10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      title: `${sample(descriptors)} ${sample(places)}`
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto error, ad delectus rerum consectetur iste quas. Praesentium beatae atque nulla ab! Placeat maiores labore iure quos corrupti quisquam soluta perspiciatis?",
+      price
     });
     await camp.save();
   }
