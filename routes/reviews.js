a7ac33a67a3a34d8a204ba417f1e7b7c6b32d438
patrycjakalinska <kalinska@student.agh.edu.pl>
2022-07-25 20:57:20 +0200

    Break routes into separate files

---
+++ routes/reviews.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router({mergeParams: true});
+
+const Review = require("../models/review");
+const Campground = require("../models/campground");
+
+const { reviewSchema } = require("../schemas.js");
+
+const catchAsync = require("../utils/catchAsync");
+const ExpressError = require("../utils/ExpressError");
+
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const message = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(message, 400);
+  } else {
+    next();
+  }
+};
+
+router.post(
+  "/",
+  validateReview,
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground.id}`);
+  })
+);
+
+router.delete(
+  "/:reviewId",
+  catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(req.params.reviewId);
+    res.redirect(`/campgrounds/${id}`);
+  })
+);
+
+
+module.exports = router;
