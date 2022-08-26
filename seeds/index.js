e218e2b8083be55a9164d34ba64e7c577322e908
patrycjakalinska <kalinska@student.agh.edu.pl>
2022-08-26 10:21:31 +0200

    Add map to campground

--- seeds/index.js
+++ seeds/index.js
@@ -22,19 +22,24 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 10) + 10;
     const camp = new Campground({
+      // your user id
       author: "62e2b9a66b9f0106aa35a98f",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       description:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto error, ad delectus rerum consectetur iste quas. Praesentium beatae atque nulla ab! Placeat maiores labore iure quos corrupti quisquam soluta perspiciatis?",
       price,
+      geometry: {
+        type: "Point",
+        coordinates: [-113.1331, 47.0202],
+      },
       images: [
         {
-          url: "https://res.cloudinary.com/ddw3shtfu/image/upload/v1661243080/YelpCamp/byqgjhwp2gu7zdjcuus9.jpg",
+          url: "https://res.cloudinary.com/ddw3shtfu/image/upload/v1661500923/YelpCamp/eirqdsxm6d4c6woeokqr.jpg",
           filename: "YelpCamp/byqgjhwp2gu7zdjcuus9",
         },
         {
-          url: "https://res.cloudinary.com/ddw3shtfu/image/upload/v1661243084/YelpCamp/vh55lgjwkrf3hmpxj8x8.jpg",
+          url: "https://res.cloudinary.com/ddw3shtfu/image/upload/v1661500640/YelpCamp/dzlkrzkp1ltdwthruf0k.jpg",
           filename: "YelpCamp/vh55lgjwkrf3hmpxj8x8",
         },
       ],
